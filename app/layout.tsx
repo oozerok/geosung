753bce93c98090e5358b16793f2105d3f523f879
jooyoung <kjyoung4102@gmail.com>
2026-03-09 16:43:11 +0900

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import "./globals.css";
 import Link from "next/link";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({ children,} : {
   children: React.ReactNode
 }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={cn("font-sans", geist.variable)}>
       <body className="bg-white text-gray-900">
 
         <nav className="flex justify-between items-center px-10 py-5 border-b">
